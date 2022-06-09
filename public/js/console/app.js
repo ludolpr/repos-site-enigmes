de7d91bffb5fb5d802d0fd1f3b052ba5e863070c
ludolpr <ludolpr@gmail.com>
2022-06-09 23:05:26 +0200

    soir 23h04 09/06

--- public/js/console/app.js
+++ public/js/console/app.js
@@ -8,3 +8,48 @@ for (i = 0;i <myNodelist.length; i++){
     span.appendChild(txt);
     myNodelist[i].appendChild(span);   
 }
+
+// click sur le boutton pour retirer l'element courent
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var diiii = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// ajoute un control quand on  click sur une list objet
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Creer une nouvelle list objet quand on click sur le bouton ajouter
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
